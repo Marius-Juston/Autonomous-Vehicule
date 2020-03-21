8d2a702bf059cae87d3635595cf4f9234f9f762b
Marius-Juston <Marius.juston@hotmail.fr>
2020-03-21 16:22:21 -0400

    Created a simple Car model
    Nothing fancy just normal kinematics

--- car.py
+++ car.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.axes import Axes
 
 
@@ -50,5 +51,47 @@ class DeterministicCar(AbstractCar):
                       vy,
                       color='red',
                       width=.25)
+
+
+class Car(DeterministicCar):
+    def __init__(self, position=None, velocity=None, acceleration=None) -> None:
+        super().__init__()
+        if velocity is None:
+            self.velocity = np.zeros(2)
+        else:
+            self.velocity = np.asarray(velocity)
+
+        if acceleration is None:
+            self.acceleration = np.zeros(2)
+        else:
+            self.acceleration = np.asarray(acceleration)
+
+        if position is None:
+            self.position = np.zeros(2)
+        else:
+            self.position = np.asarray(position)
+
+        self.positions = np.array([self.position])
+
+    def get_state(self):
+        return np.array((self.position, self.velocity, self.acceleration))
+
+    state = property(get_state)
+
+    def move(self, dt):
+        self.position = self.position + self.velocity * dt + .5 * self.acceleration * dt ** 2
+        self.velocity = self.velocity + self.acceleration * dt
+
+        self.positions = np.append(self.positions, [self.position], axis=0)
+
+    def display_world(self):
+        self._draw_world(self.positions[:, 0], self.positions[:, 1], *self.velocity)
+
+    def turn(self, dangle):
+        dangle = -np.deg2rad(dangle)
+
+        rotation_matrix = np.array([[np.cos(dangle), -np.sin(dangle)], [np.sin(dangle), np.cos(dangle)]])
+
+        self.velocity = np.matmul(rotation_matrix, self.velocity)
 
 
