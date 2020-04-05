a52f9b5aec8ad16fde0f7c465edbd63603b54f73
Marius-Juston <Marius.juston@hotmail.fr>
2020-04-05 16:41:55 -0400

    made creation and assignment in same line

--- C++/HistogramFilterOptimization/move.cpp
+++ C++/HistogramFilterOptimization/move.cpp
@@ -5,15 +5,11 @@ using namespace std;
 
 // OPTIMIZATION: Pass large variable by reference
 vector<vector<float> > move(int dy, int dx,
-    int height, width;
-    height = beliefs.size();
-    width = beliefs[0].size();
                             vector<vector<float> > &beliefs) {
+    vector<float>::size_type height = beliefs.size();
+    vector<float>::size_type width = beliefs[0].size();
 
-    vector<vector<float> > newGrid;
-
-    // OPTIMIZATION: Use improved zeros function
-    newGrid = zeros(height, width);
+    vector<vector<float> > newGrid = zeros(height, width);
 
 // OPTIMIZATION: Eliminate any variables that aren't needed
     int j, new_i, new_j;
