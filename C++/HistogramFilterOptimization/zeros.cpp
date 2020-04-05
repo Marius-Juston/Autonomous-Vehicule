7d52b0882ae9a0960bf851bc8b26f5bcbdd0f62b
Marius-Juston <Marius.juston@hotmail.fr>
2020-04-05 16:16:36 -0400

    opimized zero construction

--- C++/HistogramFilterOptimization/zeros.cpp
+++ C++/HistogramFilterOptimization/zeros.cpp
@@ -2,21 +2,7 @@
 
 using namespace std;
 
-vector<vector<float> > zeros(int height, int width) {
-    int i, j;
-
+vector<vector<float> > zeros(const int height, const int width) {
     // OPTIMIZATION: Reserve space in memory for vectors
-    vector<vector<float> > newGrid;
-    vector<float> newRow;
-
-    // OPTIMIZATION: nested for loop not needed
-    // because every row in the matrix is exactly the same
-    for (i = 0; i < height; i++) {
-        newRow.clear();
-        for (j = 0; j < width; j++) {
-            newRow.push_back(0.0);
-        }
-        newGrid.push_back(newRow);
-    }
-    return newGrid;
+    return vector<vector<float>>(height, vector<float>(width, 0));
 }
