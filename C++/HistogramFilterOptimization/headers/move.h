f415d22af54aed2a4f873cbbe93b5ed9aac97c62
Marius-Juston <Marius.juston@hotmail.fr>
2020-04-05 17:44:29 -0400

    simple reformatting
    removed unecessary imports and refromatted files

--- C++/HistogramFilterOptimization/headers/move.h
+++ C++/HistogramFilterOptimization/headers/move.h
@@ -2,9 +2,7 @@
 #define MOVE_H
 
 #include <vector>
-#include <iostream>
 
-std::vector<std::vector<float> > move(int dy, int dx,
-                                      std::vector<std::vector<float> > &beliefs);
+std::vector<std::vector<float> > move(int dy, int dx, std::vector<std::vector<float> > &beliefs);
 
 #endif /* MOVE.H */
--- C++/HistogramFilterOptimization/headers/zeros.h
+++ C++/HistogramFilterOptimization/headers/zeros.h
@@ -1,8 +1,6 @@
 #ifndef ZEROS_H
 #define ZEROS_H
 
-#include <iostream>
-#include <ctime>
 #include <vector>
 
 std::vector<std::vector<float> > zeros(unsigned int height, unsigned int width);
