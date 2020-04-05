301171e25545233165ae67b06132bd500f474d4c
Marius-Juston <Marius.juston@hotmail.fr>
2020-04-05 16:54:55 -0400

    removed unecessary comment

--- C++/HistogramFilterOptimization/move.cpp
+++ C++/HistogramFilterOptimization/move.cpp
@@ -3,13 +3,11 @@
 
 using namespace std;
 
-// OPTIMIZATION: Pass large variable by reference
 vector<vector<float> > move(int dy, int dx, vector<vector<float> > &beliefs) {
     unsigned int height = beliefs.size(), width = beliefs[0].size();
 
     vector<vector<float> > newGrid = zeros(height, width);
 
-// OPTIMIZATION: Eliminate any variables that aren't needed
     int j, new_i, new_j;
 
     for (int i = 0; i < height; i++) {
