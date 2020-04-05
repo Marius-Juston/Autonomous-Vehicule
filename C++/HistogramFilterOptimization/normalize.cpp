8974e8f96bd2350fb6d16e454ac4dbbce41c3c75
Marius-Juston <Marius.juston@hotmail.fr>
2020-04-05 17:07:36 -0400

    simple optimization
    faster way to find total value.
    Removed unesaary varibales

--- C++/HistogramFilterOptimization/normalize.cpp
+++ C++/HistogramFilterOptimization/normalize.cpp
@@ -4,27 +4,23 @@ using namespace std;
 
 vector<vector<float> > normalize(vector<vector<float> > &grid) {
     float total = 0.0;
-    int i;
-    int j;
-    vector<float> row;
-    vector<float> newRow;
-    float oldProb;
-    for (i = 0; i < grid.size(); i++) {
-        row = grid[i];
-        for (j = 0; j < row.size(); j++) {
-            oldProb = row[j];
-            total += oldProb;
+    for (const vector<float> &r: grid) {
+        for (float e : r) {
+            total += e;
         }
     }
 
-    vector<vector<float> > newGrid;
+    unsigned int i;
+    unsigned int j;
+    vector<float> newRow;
+    unsigned int height = grid.size(), width = grid[0].size();
 
-    for (i = 0; i < grid.size(); i++) {
-        vector<float> row = grid[i];
+    vector<vector<float >> newGrid;
+
+    for (i = 0; i < height; i++) {
         newRow.clear();
-        for (j = 0; j < row.size(); j++) {
-            float oldProb = row[j];
-            float newProb = oldProb / total;
+        for (j = 0; j < width; j++) {
+            float newProb = grid[i][j] / total;
             newRow.push_back(newProb);
         }
         newGrid.push_back(newRow);
