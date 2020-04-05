fafbeabff4ce77e935968306fc8633190b5d9cc1
Marius-Juston <Marius.juston@hotmail.fr>
2020-04-05 16:25:57 -0400

    very minor initialize belief optimization

--- C++/HistogramFilterOptimization/initialize_beliefs.cpp
+++ C++/HistogramFilterOptimization/initialize_beliefs.cpp
@@ -2,28 +2,9 @@
 
 using namespace std;
 
-// OPTIMIZATION: pass large variables by reference
 vector<vector<float> > initialize_beliefs(vector<vector<char> > &grid) {
-
-    // OPTIMIZATION: Which of these variables are necessary?
-    // OPTIMIZATION: Reserve space in memory for vectors
     vector<float>::size_type height = grid.size();
     vector<float>::size_type width = grid[0].size();
 
-    vector<vector<float> > newGrid;
-    vector<float> row;
-    newGrid.reserve(height);
-    row.reserve(width);
-
-    float prob_per_cell = 1.0f / (height * width);
-
-    for (int i = 0; i < width; ++i) {
-        row.push_back(prob_per_cell);
-    }
-
-    for (int i = 0; i < height; ++i) {
-        newGrid.push_back(row);
-    }
-
-    return newGrid;
+    return vector<vector<float>>(height, vector<float>(width, 1. / (width * height)));
 }
