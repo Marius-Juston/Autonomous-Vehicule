0be0ad16fa237686c21802ca5ec0601217c6a9a5
Marius-Juston <Marius.juston@hotmail.fr>
2020-05-14 23:08:53 -0400

    Simple reformatting
    Added type set and removed unesseary comments

--- Python/route_planner/planner.py
+++ Python/route_planner/planner.py
@@ -1,8 +1,8 @@
-# without problems
 import math
+from typing import List, Dict, Set
 
 
-class PathPlanner():
+class PathPlanner:
     """Construct a PathPlanner Object"""
 
     def __init__(self, world_map, start=None, goal=None):
@@ -69,8 +69,6 @@ class PathPlanner():
                 if not neighbor in self.openSet:  # Discover a new node
                     self.openSet.add(neighbor)
 
-                # The distance from start to a neighbor
-                # the "dist_between" function may vary as per the solution requirements.
                 if self.get_tentative_gScore(current, neighbor) >= self.get_gScore(neighbor):
                     continue  # This is not a better path.
 
@@ -80,38 +78,31 @@ class PathPlanner():
         self.path = None
         return False
 
-    def create_closedSet(self):
+    def create_closedSet(self) -> Set[int]:
         """ Creates and returns a data structure suitable to hold the set of nodes already evaluated"""
-        # EXAMPLE: return a data structure suitable to hold the set of nodes already evaluated
         return set()
 
-    def create_openSet(self):
+    def create_openSet(self) -> Set[int]:
         """ Creates and returns a data structure suitable to hold the set of currently discovered nodes
         that are not evaluated yet. Initially, only the start node is known."""
         if self.start is not None:
-            # TODO: return a data structure suitable to hold the set of currently discovered nodes
-            # that are not evaluated yet. Make sure to include the start node.
-            #         return PriorityQueue((0, self.start))
-            #         print(self.start)
             return {self.start}
         raise (
             ValueError,
             "Must create start node before creating an open set. Try running PathPlanner.set_start(start_node)")
 
-    def create_cameFrom(self):
+    def create_cameFrom(self) -> Dict[int, int]:
         """Creates and returns a data structure that shows which node can most efficiently be reached from another,
         for each node."""
-        # TODO: return a data structure that shows which node can most efficiently be reached from another,
-        # for each node.
         return dict()
 
-    def create_gScore(self):
+    def create_gScore(self) -> Dict[int, float]:
         """Creates and returns a data structure that holds the cost of getting from the start node to that node,
         for each node. The cost of going from start to start is zero."""
 
         return {self.start: 0}
 
-    def create_fScore(self):
+    def create_fScore(self) -> Dict[int, float]:
         """Creates and returns a data structure that holds the total cost of getting from the start node to the goal
         by passing by that node, for each node. That value is partly known, partly heuristic.
         For the first node, that value is completely heuristic."""
@@ -123,7 +114,6 @@ class PathPlanner():
         self._reset()
         self.start = None
         self.goal = None
-        # TODO: Set map to new value.
 
     def set_start(self, start):
         """Method used to set start attribute """
@@ -135,61 +125,48 @@ class PathPlanner():
         self._reset()
         self.goal = goal
 
-    def is_open_empty(self):
+    def is_open_empty(self) -> bool:
         """returns True if the open set is empty. False otherwise. """
         return not self.openSet
 
-    def get_current_node(self):
+    def get_current_node(self) -> int:
         """ Returns the node in the open set with the lowest value of f(node)."""
-        # TODO: Return the node in the open set with the lowest value of f(node).
-
         a = {node: self.fScore[node] for node in self.openSet}
 
         return min(a, key=a.get)
 
-    def get_neighbors(self, node):
+    def get_neighbors(self, node) -> List[int]:
         """Returns the neighbors of a node"""
         return self.map.roads[node]
 
-    def get_gScore(self, node):
+    def get_gScore(self, node) -> float:
         """Returns the g Score of a node"""
         if node not in self.gScore:
             return float('infinity')
 
         return self.gScore[node]
-        # TODO: Return the g Score of a node
 
-    # In[37]:
-
-    def distance(self, node_1, node_2):
+    def distance(self, node_1, node_2) -> float:
         """ Computes the Euclidean L2 Distance"""
-        # TODO: Compute and return the Euclidean L2 Distance
         node_1 = self.map.intersections[node_1]
         node_2 = self.map.intersections[node_2]
 
         return math.hypot((node_1[0] - node_2[0]), (node_1[1] - node_2[1]))
 
-    def get_tentative_gScore(self, current, neighbor):
+    def get_tentative_gScore(self, current, neighbor) -> float:
         """Returns the tentative g Score of a node"""
-        # TODO: Return the g Score of the current node
-        # plus distance from the current node to it's neighbors
         return self.get_gScore(current) + self.distance(current, neighbor)
 
-    def heuristic_cost_estimate(self, node):
+    def heuristic_cost_estimate(self, node) -> float:
         """ Returns the heuristic cost estimate of a node """
-        # TODO: Return the heuristic cost estimate of a node
         return self.distance(node, self.goal)
 
-    def calculate_fscore(self, node):
+    def calculate_fscore(self, node) -> float:
         """Calculate the f score of a node. """
-        # TODO: Calculate and returns the f score of a node.
-        # REMEMBER F = G + H
         return self.get_gScore(node) + self.heuristic_cost_estimate(node)
 
-    def record_best_path_to(self, current, neighbor):
+    def record_best_path_to(self, current, neighbor) -> None:
         """Record the best path to a node """
-        # TODO: Record the best path to a node, by updating cameFrom, gScore, and fScore
-        print(current, neighbor)
         self.cameFrom[neighbor] = current
         self.gScore[neighbor] = self.get_tentative_gScore(current, neighbor)
         self.fScore[neighbor] = self.calculate_fscore(neighbor)
